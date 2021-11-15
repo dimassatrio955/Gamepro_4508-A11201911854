deb8233634e60b888baf390f95cb683292a93895
dimassatrio955 <dimassatrio955@gmail.com>
2021-11-15 23:53:59 +0700

    fisrt

---
+++ UTS/Piring Terbang - Dimas Satrio Utomo/Assets/Bullet_Controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int speed = 100;
+
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = Vector2.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "enemy")
+        {
+            ScoreManager.score += 10;
+            Destroy(gameObject);
+            playExplosion();
+        }
+    }
+
+    private void playExplosion()
+    {
+        GameObject e = Instantiate(explosion) as GameObject;
+        e.transform.position = transform.position;
+
+        Destroy(e, 2.0f);
+
+    }
+}
+
+
